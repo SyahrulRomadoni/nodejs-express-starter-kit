77ad34b269207cee216c7358c62b2f239fc68954
Alfredo09011 <sc.alfredo@akebono-astra.co.id>
2024-10-29 09:04:56 +0700

    update

--- app/config/config.js
+++ app/config/config.js
@@ -4,24 +4,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
+    host: process.env.DB_HOST_DEV         || '127.0.0.1',
+    dialect: process.env.DB_DIALECT_DEV   || 'mysql',
     username: process.env.DB_USERNAME_DEV || 'root',
     password: process.env.DB_PASSWORD_DEV || null,
     database: process.env.DB_NAME_DEV     || 'nodejs_express_db',
-    host: process.env.DB_HOST_DEV         || '127.0.0.1',
-    dialect: process.env.DB_DIALECT_DEV   || 'mysql',
   },
   staging: {
+    host: process.env.DB_HOST_STG         || '127.0.0.1',
+    dialect: process.env.DB_DIALECT_STG   || 'mysql',
     username: process.env.DB_USERNAME_STG || 'root',
     password: process.env.DB_PASSWORD_STG || null,
     database: process.env.DB_NAME_STG     || 'nodejs_express_db_test',
-    host: process.env.DB_HOST_STG         || '127.0.0.1',
-    dialect: process.env.DB_DIALECT_STG   || 'mysql',
   },
   production: {
+    host: process.env.DB_HOST_PRO         || '127.0.0.1',
+    dialect: process.env.DB_DIALECT_PRO   || 'mysql',
     username: process.env.DB_USERNAME_PRO || 'root',
     password: process.env.DB_PASSWORD_PRO || null,
     database: process.env.DB_NAME_PRO     || 'nodejs_express_db_prod',
-    host: process.env.DB_HOST_PRO         || '127.0.0.1',
-    dialect: process.env.DB_DIALECT_PRO   || 'mysql',
   },
 };
