90a27ffb0ebce8e56115bdc88c0be8d29d89adb5
Alfredo09011 <sc.alfredo@akebono-astra.co.id>
2024-10-30 09:41:43 +0700

    update

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -3,8 +3,7 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
-const db = require('../models');
-const { Users } = db;
+const { Users } = require('../models');
 const { addToBlacklist } = require('../middlewares/tokenBlacklist');
 
 exports.register = async (req, res) => {
