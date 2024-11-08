3b09b121cebb008e97b56ceccb35af99ece5bf0a
Alfredo09011 <sc.alfredo@akebono-astra.co.id>
2024-11-08 15:40:43 +0700

    Update

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -68,7 +68,7 @@ exports.login = async (req, res) => {
                 uuid_role: users.uuid_role,
             },
             process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRATION || '1440m' }
+            { expiresIn: process.env.JWT_EXPIRESIN || '1440m' }
         );
 
         res.json({
