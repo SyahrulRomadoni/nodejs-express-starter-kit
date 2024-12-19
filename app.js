ccfc2ffaeab05aca1bd37bbf43e359d0791114ad
Alfredo0911 <sc.alfredo@akebono-astra.co.id>
2024-12-19 11:36:27 +0700

    Add new library CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Import cors
 const authRoutes = require('./app/routes/authRoutes');
 const userRoutes = require('./app/routes/userRoutes');
 const roleRoutes = require('./app/routes/roleRoutes');
@@ -7,8 +8,21 @@ const roleRoutes = require('./app/routes/roleRoutes');
 dotenv.config();
 
 const app = express();
+
+// Aktifkan middleware CORS untuk semua rute
+app.use(cors()); 
+
+// Atau jika ingin menggunakan konfigurasi khusus:
+// const corsOptions = {
+//   origin: 'http://localhost:3000', // Ganti dengan domain yang diizinkan
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+// app.use(cors(corsOptions));
+
 app.use(express.json());
 
+// Definisikan routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/roles', roleRoutes);
